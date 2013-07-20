65a180bba0a959fa778658b2a551ca7d3ccd1741
crespo180 <matan180@gmail.com>
2013-07-20 14:58:01 +0300

    added message manager

---
+++ Kindergarten/Kindergarten.BL/Messages/MessagesManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kindergarten.BL.Messages
+{
+    class MessagesManager
+    {
+    }
+}
