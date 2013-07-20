a08f48d6f6bbbc882cd4bae3293217694df82565
crespo180 <matan180@gmail.com>
2013-07-20 13:41:32 +0300

    ff

---
+++ Kindergarten/Kindergarten.Domain/Entities/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kindergarten.Domain.Entities
+{
+    public class Message
+    {
+        public Person Sender { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime SendTime { get; set; }
+    }
+}
---
+++ Kindergarten/Kindergarten.Domain/Entities/MessageReceiver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kindergarten.Domain.Entities
+{
+    public class MessageRecipient
+    {
+        public Person Recipient { get; set; }
+        public Message Message { get; set; }
+    }
+}
