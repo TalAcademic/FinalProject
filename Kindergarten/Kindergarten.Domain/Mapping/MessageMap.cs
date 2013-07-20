1295e254ed3d3de189031ac0d30c33340d438033
crespo180 <matan180@gmail.com>
2013-07-20 14:25:31 +0300

    dfd

--- Kindergarten/Kindergarten.Domain/Mapping/MessageMap.cs
+++ Kindergarten/Kindergarten.Domain/Mapping/MessageMap.cs
@@ -16,6 +16,7 @@ namespace Kindergarten.Domain.Mapping
             Map(x => x.Body);
             Map(x => x.SendTime);
             References(x => x.Sender);
+            References(x => x.Sender);
             HasManyToMany(x => x.Recipients).Cascade.All();
         }
     }
